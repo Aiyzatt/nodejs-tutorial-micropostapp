d892a77a01a12917b651700978ebf5cd18f2536b
Aiyzatt <a.sassa92@gmail.com>
2022-11-18 21:59:22 +0900

    feat: アカウント登録実装

--- src/micropost/routes/accounts.js
+++ src/micropost/routes/accounts.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const globalConfig = require('../config/global');
+const knex = require('../db/knex');
+const bcrypt = require('bcrypt');
 
 router.get('/', function(req, res, next) {
   res.redirect('/');
@@ -8,9 +10,50 @@ router.get('/', function(req, res, next) {
 
 /* GET ユーザ登録 */
 router.get('/signup', function(req, res, next) {
+  console.log(req.flash());
   res.render('accounts/signup', {
     title: globalConfig.appName + ' | ' + 'アカウント登録',
   });
+});
+
+/* POST ユーザ登録 */
+router.post('/signup', async function(req, res, next) {
+  const posts = req.body;
+
+  knex('users')
+    .select('*')
+    .where('username', posts.username)
+    .then(async (results) => {
+      if (results.length > 0) {
+        req.flash('error', 'このユーザ名はすでに使われています。');
+        res.redirect('/accounts/signup');
+      } else if (posts.password !== posts.repassword) {
+        req.flash('error', 'パスワードが一致しません。');
+        res.redirect('/accounts/signup');
+      } else {
+        const hashedPassword = await bcrypt.hash(posts.password, 10);
+        knex('users')
+          .insert({
+            username: posts.username,
+            password: hashedPassword,
+            email: posts.email,
+            is_admin: 0,
+          })
+          .then((results) => {
+            req.session.userid = results[0];
+            res.redirect('/');
+          })
+          .catch((error) => {
+            req.flash('error', 'ユーザ登録ができませんでした: ' + error.sqlMessage);
+            res.redirect('/accounts/signup');
+          });
+      }
+    })
+    .catch((error) => {
+      req.flash('error', 'ユーザ登録ができませんでした: ' + error.sqlMessage);
+      res.redirect('/accounts/signup');
+    });
+
 });
 
 /* GET ログイン */
